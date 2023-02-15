b8130d736b4329b9e6245f275c0141ec3d42a341
bh3605 <bdhair198@aol.com>
2023-02-15 16:13:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# scope-global-styles
+
+[![NPM version][npm-image]][npm-url]
+[![License][license-image]][license-url]
+[![Downloads][downloads-image]][downloads-url]
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage with Webpack](#usage-with-webpack)
+- [Options](#options)
+- [Contribute](#contribute)
+- [License](#license)
+
+## Install
+
+```console
+$ npm install scope-global-styles
+```
+
+
+## Usage with Webpack
+```js
+const globalStylePrefixer = require('scope-global-styles');
+globalStylePrefixer("my-prefix", webpack, require("./angular.json"));
+return webpack;
+```
+
+## Options
+
+| Name | Type | Description |
+| prefix | string | The string to prepend to every CSS selector. |
+| webpack | object | Webpack configuration object |
+| ngJson | object | json object of your angular.json file |
+
+## Contribute
+
+Please contribute! If you have any questions or bugs, [open an issue](https://github.com/bh3605/scope-global-styles/issues/new), or open a pull request with a fix.
+
+
+## License
+[MIT](LICENSE) @ 2023 Bryson Hair
---
+++ index.js
@@ -0,0 +1,105 @@
+module.exports = function scopeGlobalStypes(prefix, webpackConfig, ngJson) {
+    const projectName = singleSpaWebpackConfig.output.uniqueName;
+    const project = ngJson.projects[projectName];
+    if(!project || project.architect?.build?.options?.styles.length < 1) {
+        return;
+    }
+    const { entryPoints, noInjectNames } = normalizeGlobalStyles(project.architect.build.options.styles);
+    const filePathRoot = webpackConfig.resolve.roots[0] + "\\";
+    for(let i = 0; i < entryPoints.styles.length; i++) {
+        entryPoints.styles[i] = filePathRoot + entryPoints.styles[i].replace("/", "\\");
+    }
+
+    const postcssPrefixSelector = {
+        "postcss-prefix-selector": {
+            prefix,
+            transform(prefix, selector, prefixedSelector, filePath, rule) {
+                if(selector.match(/^(html|body)/)) {
+                    return selector.replace(/^([^\s]*)/, `$1 ${prefix}`);
+                }
+
+                if(filePath.match(/node_modules/)) {
+                    return selector; // Do not prefix styles imported from node_modules
+                }
+
+                const annotation = rule.prev();
+                if(annotation && annotation.type === 'comment' && annotation.text.trim() === 'no-prefix') {
+                    return selector; // Do not prefix style rules that are preceded by: /* no-prefix */
+                }
+
+                return prefixedSelector;
+            },
+            includeFiles: entryPoints.styles
+        }
+    };
+
+    const postcssLoader = require.resolve('postcss-loader');
+    const usePostCssLoader = {
+        loader: postcssLoader,
+        options: {
+            postcssOptions: {
+                plugins: {
+                    ...postcssPrefixSelector
+                }
+            }
+        }
+    }
+
+    const postCssAddPrefixSelectorRule = {
+        test: /\.css$/,
+        use: [
+            usePostCssLoader
+        ]
+    };
+    const scssPostCssAddPrefixSelectorRule = {
+        test: /\.scss$/,
+        use: [
+            usePostCssLoader,
+            'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+    };
+    webpackConfig.module.rules.push(postCssAddPrefixSelectorRule, scssPostCssAddPrefixSelectorRule);
+}
+
+function normalizeGlobalStyles (styleEntrypoints) {
+    const entryPoints = {};
+    const noInjectNames = [];
+
+    if(styleEntrypoints.length === 0) {
+        return { entryPoints, noInjectNames };
+    }
+
+    for(const style of normalizeExtraEntryPoints(styleEntrypoints, 'styles')) {
+        // Add style entry points.
+        entryPoints[style.bundleName] !== null && entryPoints[style.bundleName] !== void 0 ? entryPoints[style.bundleName] : (entryPoints[style.bundleName] = []);
+        entryPoints[style.bundleName].push(style.input);
+
+        // Add non injected styles to the list.
+        if(!style.inject) {
+            noInjectNames.push(style.bundleName);
+        }
+    }
+
+    return { entryPoints, noInjectNames };
+}
+
+function normalizeExtraEntryPoints (extraEntryPoints, defaultBundleName,) {
+    return extraEntryPoints.map((entry) => {
+        if(typeof entry === 'string') {
+            return { input: entry, inject: true, bundleName: defaultBundleName };
+        }
+
+        const { inject = true, ...newEntry } = entry;
+        let bundleName;
+        if(entry.bundleName) {
+            bundleName = entry.bundleName;
+        } else if(!inject) {
+            // Lazy entry points use the file name as bundle name.
+            bundleName = path.parse(entry.input).name;
+        } else {
+            bundleName = defaultBundleName;
+        }
+
+        return { ...newEntry, inject, bundleName };
+    });
+}
